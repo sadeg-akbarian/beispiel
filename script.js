1f759cf7609f2e7ccdd79c303d7665e0600b3855
sadeg-akbarian <sadeg.akbarian@gmail.com>
2023-08-02 19:59:01 +0200

    feat: 31.Juli

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-window.console.log("Hallo");
+function logEvent(e) {
+  console.log(e.target);
+  console.log(e.currentTarget);
+  console.log(e.eventPhase);
+}
+
+document.querySelector("body").addEventListener("click", logEvent);
